ce3a82d7d877e0e88c1cde42c9edc15cc7128aa2
Harrison Doll <hdoll2@wisc.edu>
2024-12-01 20:16:31 -0600

    wfs

--- solution/wfs.c
+++ solution/wfs.c
@@ -1,4 +1,13 @@
-#define FUSE_USE_VERSION 30
+#include <fuse.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include "wfs.h"
+
 
 int main(int argc, char *argv[]) 
 {
